2b977f8beb0dcefd18096fefebe6c120948a5982
juliangranda <juliangranda@hotmail.com>
2021-09-26 20:02:14 -0500

    implementacion de nuevas funcionalidades de botones

--- flutter/contador/lib/src/pages/contador_page.dart
+++ flutter/contador/lib/src/pages/contador_page.dart
@@ -19,15 +19,16 @@ solo se puede usar en este archivo y no en otro ,es decir, lo estamos volviendo 
 /*
 
 nota importante: un statefulwidget necesita de : 
-1)una clase que crea el estado para sobreescribir el cambio
-2)construir un widget 
-3)setstate para redibujar el widget
+1)una clase que crea el estado para sobreescribir el cambio es decir un statefulwidget
+2)otra clase que extiende ese widget
+3)setstate para redibujar el widget y permita ver el cambio de estado 
 
 */
 class _ContadorPageState extends State<ContadorPage>{
 
   final _estiloTexto = new TextStyle(fontSize: 30);
 
+//las variables ya se declaran como int, double,string ,etc pq al ser un statefulwidget su estado puede cambiar
   int _conteo = 10;
 
   @override
@@ -53,19 +54,39 @@ class _ContadorPageState extends State<ContadorPage>{
   }
 
 
-//este widget lo estamos haciendo para dejar el codigo mas ordenado y legible para que no sea tan dificil de leer
+//este widget o metodolo estamos haciendo para dejar el codigo mas ordenado y legible para que no sea tan dificil de leer
   Widget _crearBotones(){
     return Row(
       mainAxisAlignment: MainAxisAlignment.end,
       children: <Widget>[
       SizedBox(width: 30),
-      FloatingActionButton(onPressed: () {  }, child: Icon(Icons.exposure_zero)),
+      FloatingActionButton(child: Icon(Icons.exposure_zero), onPressed: _reset),
       Expanded(child: SizedBox()),
-      FloatingActionButton(onPressed: () {  }, child: Icon(Icons.remove)),
+      FloatingActionButton(child: Icon(Icons.remove), onPressed: _sustraer),
       SizedBox(width: 5.0),
-      FloatingActionButton(onPressed: () {  }, child: Icon(Icons.add)),
+      FloatingActionButton(child: Icon(Icons.add), onPressed: _agregar), 
     ],);
     
     
+  }
+
+/*
+nota importante:
+
+al momento de añadir la funcion o widget al onPressed del boton no se debe de poner los parentesis() pq no necesitamos que se ejecute una funcion cuando se crea el boton o ejecuta la app
+sino que necesitamos que se ejecute la funcion cuando se le da click al boton entonces por esto va sin parentesis el llamado de la funcion o widget
+ */
+  void _agregar(){
+    
+    setState(() => _conteo++);
+  }
+
+
+  void _sustraer(){
+    setState(() => _conteo--);
+  }
+
+  void _reset(){
+    setState(() => _conteo=0);
   }
 }
--- flutter/contador/lib/src/pages/home_page.dart
+++ flutter/contador/lib/src/pages/home_page.dart
@@ -3,6 +3,9 @@ import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget{
 
+
+//nota: las variables van con final pq es un stateleeswidget dado que no tiene un estado y hay que recordar que la palabra final es para
+//decir que solo se ejecute la primera vez y sea inmutable como una constante por lo tanto no cambia de estado y por eso debe de ser declarada de esa manera
   final estiloTexto = new TextStyle(fontSize: 30);
 
   final conteo = 10;
