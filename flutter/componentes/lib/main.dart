14ea5cd546aa9be5f8086c370f2805adf7ad375d
juliangranda <juliangranda@hotmail.com>
2021-09-27 10:12:42 -0500

    home page

--- flutter/componentes/lib/main.dart
+++ flutter/componentes/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:componentes/src/pages/home_page.dart';
 import 'package:componentes/src/pages/home_temp.dart';
 import 'package:flutter/material.dart';
 
@@ -9,7 +10,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Componentes',
       debugShowCheckedModeBanner: false,
-      home: HomePageTemp()
+      home: HomePage()
     );
   }
 }
