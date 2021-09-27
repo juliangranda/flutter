31966e3b43412cd34e155986a311198e4331bbe6
juliangranda <juliangranda@hotmail.com>
2021-09-27 09:47:29 -0500

    listTiles

--- flutter/componentes/lib/src/pages/home_temp.dart
+++ flutter/componentes/lib/src/pages/home_temp.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 class HomePageTemp extends StatelessWidget {
-  const HomePageTemp({Key? key}) : super(key: key);
+  //const HomePageTemp({Key? key}) : super(key: key);
+
+  final opciones = ['Uno','Dos','Tres','Cuatro','Cinco'];
 
   @override
   Widget build(BuildContext context) {
@@ -10,20 +12,28 @@ class HomePageTemp extends StatelessWidget {
         title: Text('Componentes temp'),
       ),
       body: ListView(
-        children: <Widget>[
-          ListTile(
-            title: Text('listtile title'),
-          ),
-          Divider(),
-          ListTile(
-            title: Text('listtile title'),
-          ),
-          Divider(),
-          ListTile(
-            title: Text('listtile title'),
-          ),
-        ],
+        children: _crearItems()
+        
       ),
     );
+  }
+
+  //lista de widget
+  List<Widget> _crearItems(){
+
+    List<Widget> lista = <Widget>[];
+
+    for (String opt in opciones) {
+      
+      final tempWidget = ListTile(
+        title: Text( opt ),
+      );
+
+//el .. hace referencia a la lista y se usa para que el se puedan añadir metodos mas limpios
+      lista..add( tempWidget )
+          ..add( Divider() );
+      
+    }
+    return lista;
   }
 }
