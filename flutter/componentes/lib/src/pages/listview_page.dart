72f6d20ecc148a68f02639cf4619605abbf73560
juliangranda <juliangranda@hotmail.com>
2021-09-28 17:32:00 -0500

    infinite scroll con error

--- flutter/componentes/lib/src/pages/listview_page.dart
+++ flutter/componentes/lib/src/pages/listview_page.dart
@@ -1,42 +1,100 @@
+import 'dart:async';
+ 
 import 'package:flutter/material.dart';
-
+ 
 class ListaPage extends StatefulWidget {
-  //ListaPage({Key? key}) : super(key: key);
-
   @override
   _ListaPageState createState() => _ListaPageState();
 }
-
+ 
 class _ListaPageState extends State<ListaPage> {
-
-  List<int> _listaNumeros = [1,2,3,4,5];
-
+  ScrollController _scrollController = new ScrollController();
+  List<int> _listaNumeros = [];
+  int _ultimoItem = 0;
+  bool _isloading = false;
+  @override
+  void initState() {
+    super.initState();
+    _agregar10();
+    _scrollController.addListener(() {
+      if (_scrollController.position.pixels ==
+          _scrollController.position.maxScrollExtent) {
+        _fetchData();
+      }
+    });
+  }
+ 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Listas'),
+        title: Text("ListView Page"),
+        backgroundColor: Colors.blueGrey[800],
       ),
-      body: _crearLista(),
+      body: Stack(
+        children: [_crearLista(), _crearLoading()],
+      ),
     );
   }
-
-
-  Widget _crearLista(){
-    //listviewbuilder se va a encargar de renderizar los elementos cuando sea necesario o es el encargado de redibujar los elementos
+ 
+  Widget _crearLista() {
     return ListView.builder(
-      
+      controller: _scrollController,
       itemCount: _listaNumeros.length,
-      //builder:la forma como se va a dibujar este elemento
-      itemBuilder: (BuildContext context, int index){
-
+      itemBuilder: (BuildContext context, int index) {
         final imagen = _listaNumeros[index];
         return FadeInImage(
-            placeholder: AssetImage('assets/jar-loading.gif'),
-           image: NetworkImage('https://picsum.photos/500/300/?image=$imagen')
-           );
+            image: NetworkImage("https://picsum.photos/seed/$imagen/500/400"),
+            placeholder: AssetImage("assets/jar-loading.gif"));
       },
-
+    );
+  }
+ 
+  void _agregar10() {
+    for (var i = 1; i < 5; i++) {
+      _ultimoItem++;
+      _listaNumeros.add(_ultimoItem);
+      setState(() {});
+    }
+  }
+ 
+  Future _fetchData() async {
+    _isloading = true;
+    setState(() {});
+    final _duracion = new Duration(seconds: 5);
+    return new Timer(_duracion, respuestaHTTP);
+  }
+ 
+  void respuestaHTTP() {
+    _isloading = false;
+    _scrollController.animateTo(_scrollController.position.pixels + 100,
+        curve: Curves.fastOutSlowIn, duration: Duration(milliseconds: 250));
+    _agregar10();
+  }
+ 
+  Widget _crearLoading() {
+    if (_isloading == true) {
+      return Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [CircularProgressIndicator()],
+          ),
+          SizedBox(
+            height: 15.0,
+          ),
+        ],
       );
+    } else {
+      return Container();
+    }
+  }
+ 
+  @override
+  void dispose() {
+    super.dispose();
+    _scrollController.dispose();
   }
 }
