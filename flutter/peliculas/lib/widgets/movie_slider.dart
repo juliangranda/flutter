4b058c9b1294c77cd10eb1dbaff182e07291d008
juliangranda <juliangranda@hotmail.com>
2021-09-29 11:34:33 -0500

    movie slider

--- flutter/peliculas/lib/widgets/movie_slider.dart
+++ flutter/peliculas/lib/widgets/movie_slider.dart
@@ -7,8 +7,8 @@ class MovieSlider extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
-      height: 250,
-      color: Colors.red,
+      height: 260,
+      //color: Colors.red,
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
@@ -18,6 +18,7 @@ class MovieSlider extends StatelessWidget {
             child: Text('populares', style: TextStyle(fontSize:20, fontWeight: FontWeight.bold)),
 
             ),
+            SizedBox(height:5),
           //se usa un widget llamado expanded para que tenga la listView.builder para que esa list view sepa que tañado debe de tomar al ser el tamaño de la columna flexible por el padding,etc
           Expanded(
             child: ListView.builder(
@@ -42,8 +43,34 @@ class _MoviePoster extends StatelessWidget {
     return Container(
                   width: 130,
                   height: 190,
-                  color: Colors.green,
-                  margin: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                  //color: Colors.green,
+                  margin: EdgeInsets.symmetric(horizontal: 10),
+                  child: Column(
+                    children: [
+
+                      GestureDetector(
+                        onTap: () => Navigator.pushNamed(context, 'details',arguments: 'movie-instance'),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(20),
+                          child: FadeInImage(
+                            placeholder: AssetImage('assets/no-image.jpg'),
+                             image: NetworkImage('https://via.placeholder.com/300x400'),
+                             width: 130,
+                             height: 190,
+                             fit: BoxFit.cover,
+                             ),
+                        ),
+                      ),
+
+                        SizedBox(height: 5),
+                         Text(
+                           'star wars: el retorno del el nuevo jedi silvestre de Monte cristo',
+                           maxLines: 2,
+                           overflow: TextOverflow.ellipsis,
+                           textAlign: TextAlign.center,
+                           ),
+                    ],
+                  ),
                 );
   }
 }
