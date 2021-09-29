45e7214bca2fe208ea06adc14bd0746c46524531
juliangranda <juliangranda@hotmail.com>
2021-09-29 18:58:14 -0500

    buscando error para mostrar imagenes

--- flutter/peliculas/lib/providers/movies_provider.dart
+++ flutter/peliculas/lib/providers/movies_provider.dart
@@ -6,14 +6,21 @@ import 'package:flutter/material.dart';
 //importamos http
 import 'package:http/http.dart' as http;
 import 'package:peliculas/modals/modals.dart';
+import 'package:peliculas/modals/popular_response.dart';
 
 class MoviesProvider extends ChangeNotifier {
   String _apiKey = '37f698cc72791bec8991273ef0e1d5aa';
   String _baseUrl = 'api.themoviedb.org';
   String _language = 'es-ES';
+
+  List<Movie> onDisplayMovies = [];
+  List<Movie> popularMovies = [];
+
+//para poner a funcionar toda la informacion que estamos recibiendo
   MoviesProvider() {
     print('MoviesProvider inicializando');
     this.getOnDisplayMovies();
+    this.getPopularMovies();
   }
 
   getOnDisplayMovies() async {
@@ -28,12 +35,27 @@ class MoviesProvider extends ChangeNotifier {
     // Await the http get response, then decode the json-formatted response.
     final response = await http.get(url);
     final nowPlayingResponse = NowPlayingResponse.fromJson(response.body);
-    print(nowPlayingResponse.results[0].title);
-    //final Map<String, dynamic> decodeData = json.decode(response.body);
-    //print(decodeData['results']);
-    //if( response.statusCode != 200) return print('error');
 
+    //print(nowPlayingResponse.results[0].title);
+
+    onDisplayMovies = nowPlayingResponse.results;
+
+//le dice a todos los widgets, hey! sucedio un cambio en la data o en tal parte entonces REDIBUJALO!
+    notifyListeners();
 //ver si esta recibiendo la informacion
     //print(response.body);
   }
+
+  getPopularMovies() async{
+        var url = Uri.https( _baseUrl, '3/movie/popular', {
+      'api_key': _apiKey,
+      'language': _language,
+      'page': '1',
+    });
+    final response = await http.get(url);
+    final popularResponse = PopularResponse.fromJson(response.body);
+    popularMovies = [...popularMovies , ...popularResponse.results];
+    notifyListeners();
+  }
+  
 }
--- flutter/peliculas/lib/screens/home_screen.dart
+++ flutter/peliculas/lib/screens/home_screen.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:peliculas/providers/movies_provider.dart';
 import 'package:peliculas/widgets/widgets.dart';
+import 'package:provider/provider.dart';
 
 class HomeScreen extends StatelessWidget {
   //const DetailsScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+                                      //especifica cual provider
+    final moviesProvider = Provider.of<MoviesProvider>(context);
+    //print(moviesProvider.onDisplayMovies);
     return Scaffold(
       appBar: AppBar(title: Text('peliculas en cines'), elevation: 0, actions: [
         IconButton(
@@ -18,8 +23,8 @@ class HomeScreen extends StatelessWidget {
         child: Column(
           children: [
             //cardSwiper
-            CardSwiper(),
-
+            CardSwiper( movies: moviesProvider.onDisplayMovies),
+            
             //listado horizontal de peliculas
             MovieSlider(),
             
