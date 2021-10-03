d510130b48f890296446d5a6bc703ca0e057603c
juliangranda <juliangranda@hotmail.com>
2021-10-03 12:40:15 -0500

    formulario de login

--- flutter/productos_app/lib/screens/login_screen.dart
+++ flutter/productos_app/lib/screens/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:productos_app/widgets/widgets.dart';
 class LoginScreen extends StatelessWidget {
   
@@ -19,13 +20,61 @@ class LoginScreen extends StatelessWidget {
                     Text('login', style: Theme.of(context).textTheme.headline4,),
                     SizedBox(height:30),
 
-                    Text('formulario')
+                    //Text('formulario')
+                    _LoginForm(),
                   ],
+
                 ),
               ),
+
+              SizedBox(height: 50 ),
+              Text('crear una nueva cuenta', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold))
             ],
           ),
         )
+      ),
+    );
+  }
+}
+
+
+class _LoginForm extends StatelessWidget {
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Form(
+        //mantener la referencia al key
+        child: Column(
+          children: [
+            TextFormField(
+              autocorrect: false,
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.deepPurple
+                    ),
+
+                ),
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color:Colors.deepPurple,
+                    width: 2
+                  )
+                ),
+                //lo que aparece en el textform field visual y al dar click
+                hintText: 'john.doe@gmail.com',
+                labelText: 'Correo',
+                labelStyle: TextStyle(
+                  color: Colors.grey
+                ),
+                prefixIcon: Icon(Icons.alternate_email_sharp, color: Colors.deepPurple)
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
