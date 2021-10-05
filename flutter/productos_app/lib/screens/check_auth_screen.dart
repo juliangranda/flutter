83f4e930555e538baca27881d127a8254126e0c3
juliangranda <juliangranda@hotmail.com>
2021-10-05 13:37:35 -0500

    eliminar la transicion entre pantallas

--- flutter/productos_app/lib/screens/check_auth_screen.dart
+++ flutter/productos_app/lib/screens/check_auth_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:productos_app/screens/screens.dart';
 import 'package:productos_app/services/services.dart';
 import 'package:provider/provider.dart';
 
@@ -11,28 +12,35 @@ class CheckAuthScreen extends StatelessWidget {
     final authService = Provider.of<AuthService>(context, listen: false);
     return Scaffold(
       body: Center(
-        child: FutureBuilder(
-          future: authService.readToken(),
-           builder: (BuildContext context , AsyncSnapshot<String> snapshot){
+          child: FutureBuilder(
+              future: authService.readToken(),
+              builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+                if (!snapshot.hasData) return Text('');
 
-             
-            if(!snapshot.hasData)
-              return Text('espere');
-/*             if( snapshot.data == ''){
+                if (snapshot.data == '') {
+                  Future.microtask(() {
+                    Navigator.pushReplacement(
+                        context,
+                        PageRouteBuilder(
+                            pageBuilder: (_, __, ___) => LoginScreen(),
+                            transitionDuration: Duration(seconds: 0)));
 
-            } */
-            Future.microtask((){
-              Navigator.of(context).pushReplacementNamed('login');
-            });
-            
-            return Container();
+                    //Navigator.of(context).pushReplacementNamed('login');
+                  });
+                } else {
+                  Future.microtask(() {
+                    Navigator.pushReplacement(
+                        context,
+                        PageRouteBuilder(
+                            pageBuilder: (_, __, ___) => HomeScreen(),
+                            transitionDuration: Duration(seconds: 0)));
 
-              
+                    
+                  });
+                }
 
-
-           }
-           )
-      ),
+                return Container();
+              })),
     );
   }
 }
