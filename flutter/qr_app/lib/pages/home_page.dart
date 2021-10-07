501ef93dfb72d808b2301be7c922247310d79ab4
juliangranda <juliangranda@hotmail.com>
2021-10-07 11:20:37 -0500

    cambiar pagina dependiendo del tab seleccionado( mirar current index usando switch)

--- flutter/qr_app/lib/pages/home_page.dart
+++ flutter/qr_app/lib/pages/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:qr_app/pages/direcciones_page.dart';
+import 'package:qr_app/pages/mapas_page.dart';
 import 'package:qr_app/widgets/custom_navigatorBar.dart';
 import 'package:qr_app/widgets/scan_button.dart';
 
@@ -17,12 +19,31 @@ class HomePage extends StatelessWidget {
              ),
         ],
       ),
-      body: Center(
-        child: Text('Home page'),
-      ),
+      body: _HomePageBody(),
       bottomNavigationBar: CustomNavigationBar(),
       floatingActionButton: ScanButton(),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
+  }
+}
+
+
+class _HomePageBody extends StatelessWidget {
+ 
+
+  @override
+  Widget build(BuildContext context) {
+
+    //necesito devolver un widget de manera condicional para trabajar con TABS
+    //cambiar current index para mostrar la pagina respectiva
+    final currentIndex = 1;
+    switch(currentIndex){
+      case 0:
+        return MapasPage();
+      case 1:
+        return DireccionesPage();
+      default:
+      return MapasPage();
+    }
   }
 }
