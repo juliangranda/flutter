54a03cab2b5fdbe9a2945dbc5364a2c27b30f64c
juliangranda <juliangranda@hotmail.com>
2021-10-07 16:10:05 -0500

    actualizar registros

--- flutter/qr_app/lib/providers/db_provider.dart
+++ flutter/qr_app/lib/providers/db_provider.dart
@@ -108,4 +108,10 @@ class DBProvider{
           : [];
   }
 
+  Future<int> updateScan(ScanModel nuevoScan)async{
+    final db = await database;
+    final res = await db.update('Scans', nuevoScan.toJson(), where: 'id = ?',whereArgs: [nuevoScan.id]);
+    return res;
+  }
+
 }
