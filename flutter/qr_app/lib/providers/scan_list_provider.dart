1334fd35cb2ac52863e8fde59c96b6b6625d1c6d
juliangranda <juliangranda@hotmail.com>
2021-10-07 16:26:57 -0500

    scanProvider : centralizar la informacion de los scans

---
+++ flutter/qr_app/lib/providers/scan_list_provider.dart
@@ -0,0 +1,26 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:qr_app/providers/db_provider.dart';
+
+class ScanListProvider extends ChangeNotifier{
+
+  List<ScanModel> scans = [];
+  String tipoSeleccionado = 'http';
+
+  nuevoScan(String valor)async{
+
+    final nuevoScan = new ScanModel(valor: valor);
+    final id = await DBProvider.db.nuevoScan(nuevoScan);
+
+    //asignar el id en la base de datos al modelo
+    nuevoScan.id = id;
+    if(this.tipoSeleccionado == nuevoScan.tipo){
+      this.scans.add(nuevoScan);
+      notifyListeners();
+    }
+    
+    
+    
+  }
+}
