03dba21977bf31157a2ae1a6b570c65aff360edd
juliangranda <juliangranda@hotmail.com>
2021-10-08 09:34:17 -0500

    crear y configurar api key de google maps y mostrar googlemap en la app

--- flutter/qr_app/lib/pages/mapa_page.dart
+++ flutter/qr_app/lib/pages/mapa_page.dart
@@ -1,8 +1,24 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:qr_app/models/scan_model.dart';
 
-class MapaPage extends StatelessWidget {
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class MapaPage extends StatefulWidget {
   
+
+  @override
+  State<MapaPage> createState() => _MapaPageState();
+}
+
+class _MapaPageState extends State<MapaPage> {
+
+  final CameraPosition puntoInicial = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+  Completer<GoogleMapController> _controller = Completer();
 
   @override
   Widget build(BuildContext context) {
@@ -12,8 +28,12 @@ class MapaPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Mapa'),
       ),
-      body: Center(
-        child: Text(scan.valor),
+      body: GoogleMap(
+        mapType: MapType.hybrid,
+        initialCameraPosition: puntoInicial,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
       ),
     );
   }
