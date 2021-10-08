57cb227fbb82623de6be6a1947005df8a229b732
juliangranda <juliangranda@hotmail.com>
2021-10-07 19:15:40 -0500

    mostrar scans ui sin base de datos

--- flutter/qr_app/lib/pages/mapas_page.dart
+++ flutter/qr_app/lib/pages/mapas_page.dart
@@ -5,8 +5,15 @@ class MapasPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('mapas page'),
-    );
+    return ListView.builder(
+      itemCount: 10,
+      itemBuilder: (_, i) => ListTile(
+        leading: Icon(Icons.map, color: Theme.of(context).primaryColor,),
+        title: Text('http://...........'),
+        subtitle: Text('ID: 1'),
+        trailing: Icon(Icons.keyboard_arrow_right, color: Colors.grey),
+        onTap: () => print('abrir algo...'),
+      )
+      );
   }
 }
