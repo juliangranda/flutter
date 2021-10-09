e3e512f33f5b18a957c7ab25e4e6ffa4f73a5b54
juliangranda <juliangranda@hotmail.com>
2021-10-09 14:57:34 -0500

    proyecto newapp - newsProvider

---
+++ flutter/newsapp/lib/src/pages/tabs_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class TabsPage extends StatelessWidget {
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView(
+        physics: BouncingScrollPhysics(),
+        children: [
+          
+          Container(
+            color: Colors.red
+          ),
+          Container(
+            color: Colors.green,
+          )
+        ],
+      )
+    );
+  }
+}
