ef16b00e8dbb6285ca8ca29384c96bc04554a79c
juliangranda <juliangranda@hotmail.com>
2021-10-09 15:27:22 -0500

    provider como manejador de estado

--- flutter/newsapp/lib/src/pages/tabs_page.dart
+++ flutter/newsapp/lib/src/pages/tabs_page.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class TabsPage extends StatelessWidget {
   
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: _Paginas(),
-      bottomNavigationBar: _Navegacion(),
+    return ChangeNotifierProvider(
+      create: (_) => new _NavegacionModel(),
+      child: Scaffold(
+        body: _Paginas(),
+        bottomNavigationBar: _Navegacion(),
+      ),
     );
   }
 }
@@ -15,10 +19,16 @@ class TabsPage extends StatelessWidget {
 
 
 class _Navegacion extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
+
+    final navegacionModel = Provider.of<_NavegacionModel>(context);
+
+
     return BottomNavigationBar(
       currentIndex: 0,
+      onTap: (i) => navegacionModel.paginaActual = i,
       items: [
       BottomNavigationBarItem(icon: Icon(Icons.person_outline),title: Text('para ti')),
       BottomNavigationBarItem(icon: Icon(Icons.public),title: Text('encabezados'))
@@ -42,5 +52,21 @@ class _Paginas extends StatelessWidget {
         )
       ],
     );
+  }
+}
+
+
+
+class _NavegacionModel with ChangeNotifier {
+
+  int _paginaActual = 0;
+
+
+  int get paginaActual => this.paginaActual;
+  
+  set paginaActual( int valor){
+    this._paginaActual = valor;
+    //avisa a los widgets del valor que se va a cambiar, es decir, que se redibujen todos los widgets
+    notifyListeners();
   }
 }
