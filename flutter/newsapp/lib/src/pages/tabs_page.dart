82c728d8788dd4998bb0b3ffc48943012ef648cd
juliangranda <juliangranda@hotmail.com>
2021-10-09 15:05:59 -0500

    implementado bottomNavigationBar

--- flutter/newsapp/lib/src/pages/tabs_page.dart
+++ flutter/newsapp/lib/src/pages/tabs_page.dart
@@ -6,18 +6,41 @@ class TabsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: PageView(
-        physics: BouncingScrollPhysics(),
-        children: [
-          
-          Container(
-            color: Colors.red
-          ),
-          Container(
-            color: Colors.green,
-          )
-        ],
-      )
+      body: _Paginas(),
+      bottomNavigationBar: _Navegacion(),
+    );
+  }
+}
+
+
+
+class _Navegacion extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      currentIndex: 0,
+      items: [
+      BottomNavigationBarItem(icon: Icon(Icons.person_outline),title: Text('para ti')),
+      BottomNavigationBarItem(icon: Icon(Icons.public),title: Text('encabezados'))
+    ],);
+  }
+}
+
+class _Paginas extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return PageView(
+      //physics: BouncingScrollPhysics(),
+      physics: NeverScrollableScrollPhysics(),
+      children: [
+        
+        Container(
+          color: Colors.red
+        ),
+        Container(
+          color: Colors.green,
+        )
+      ],
     );
   }
 }
