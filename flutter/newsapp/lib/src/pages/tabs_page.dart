e1762af1ac4f5170ad1faa610cb0d5ce25b11bf8
juliangranda <juliangranda@hotmail.com>
2021-10-09 17:04:54 -0500

    obtener los headlines - recibir datos por http

--- flutter/newsapp/lib/src/pages/tabs_page.dart
+++ flutter/newsapp/lib/src/pages/tabs_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:newsapp/src/services/news_service.dart';
+import 'package:newsapp/src/pages/tab1_page.dart';
 import 'package:provider/provider.dart';
 
 class TabsPage extends StatelessWidget {
@@ -49,9 +49,7 @@ class _Paginas extends StatelessWidget {
       physics: NeverScrollableScrollPhysics(),
       children: [
         
-        Container(
-          color: Colors.red
-        ),
+        Tab1Page(),
         Container(
           color: Colors.green,
         )
--- flutter/newsapp/lib/src/services/news_service.dart
+++ flutter/newsapp/lib/src/services/news_service.dart
@@ -2,7 +2,10 @@
 
 import 'package:flutter/material.dart';
 import 'package:newsapp/src/models/news_models.dart';
+import 'package:http/http.dart' as http;
 
+final _URL_NEWS = 'https://newsapi.org/v2';
+final _APIKEY = 'a76e3a6f008e4098bc5cee40a81c8524';
 class NewsService with ChangeNotifier{
 
   List<Article> headlines = [];
@@ -13,7 +16,16 @@ class NewsService with ChangeNotifier{
 
   }
 
-  getTopHeadLines(){
-    print('Cargando headLines...');
+  getTopHeadLines()async{
+    
+      final url = Uri.parse('$_URL_NEWS/top-headlines?apiKey=$_APIKEY&country=ca');
+      final resp = await http.get(url);
+
+      final newsResponse = newsResponseFromJson( resp.body );
+
+      //llamo todos los articulos con metodo http
+      this.headlines.addAll(newsResponse.articles);
+      notifyListeners();
+
   }
 }
