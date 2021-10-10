381d162b924c5aa18cfbc7238485028d8ae3a296
juliangranda <juliangranda@hotmail.com>
2021-10-10 18:33:34 -0500

    optimizaciones

--- flutter/disenos/lib/widgets/card_table.dart
+++ flutter/disenos/lib/widgets/card_table.dart
@@ -48,8 +48,31 @@ class _SingleCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var column = Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CircleAvatar(
+                  backgroundColor: this.color,
+                  child: Icon(this.icon, size:35, color: Colors.white,),
+                  radius: 30,
+                ),
+                SizedBox(height: 10,),
+                Text(this.text, style: TextStyle(color: this.color, fontSize: 18))
+              ],
+            );
+    return _CardBackground(child: column);
+  }
+}
+
+class _CardBackground extends StatelessWidget {
+  
+  final Widget child;
+
+  const _CardBackground({Key? key, required this.child}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
     return Container(
-      
       margin: EdgeInsets.all(15),
       child: ClipRRect(
         borderRadius: BorderRadius.circular(20),
@@ -62,18 +85,7 @@ class _SingleCard extends StatelessWidget {
               color: Color.fromRGBO(62, 66, 107, 0.7),
               borderRadius: BorderRadius.circular(20)
             ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                CircleAvatar(
-                  backgroundColor: this.color,
-                  child: Icon(this.icon, size:35, color: Colors.white,),
-                  radius: 30,
-                ),
-                SizedBox(height: 10,),
-                Text(this.text, style: TextStyle(color: this.color, fontSize: 18))
-              ],
-            ),
+            child: this.child,
           ),
         ),
       ),
