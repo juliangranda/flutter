501a10d55456c6fc2dc5d81a6bd89c32dd1675d2
juliangranda <juliangranda@hotmail.com>
2021-10-09 20:15:15 -0500

    obtener noticias por categoria

--- flutter/newsapp/lib/src/services/news_service.dart
+++ flutter/newsapp/lib/src/services/news_service.dart
@@ -12,6 +12,9 @@ class NewsService with ChangeNotifier{
 
   List<Article> headlines = [];
 
+  String _selectedCategory = 'business';
+
+
   List<Category> categories = [
     Category( FontAwesomeIcons.building, 'business'),
     Category( FontAwesomeIcons.tv, 'entertainment'),
@@ -22,12 +25,29 @@ class NewsService with ChangeNotifier{
     Category( FontAwesomeIcons.memory, 'technology'),
   ];
   
+  Map<String, List<Article>> categoryArticles ={
+
+  };
 
   NewsService(){
 
     this.getTopHeadLines();
 
+    categories.forEach((item) { 
+      this.categoryArticles[item.name] = [];
+    });
+
   }
+
+  String get selectedCategory => this._selectedCategory;
+  set selectedCategory(String valor){
+
+    this._selectedCategory = valor;
+    
+    this.getArticlesByCategory(valor);
+    notifyListeners();
+  }
+
 
   getTopHeadLines()async{
     
@@ -41,5 +61,20 @@ class NewsService with ChangeNotifier{
       this.headlines.addAll(newsResponse.articles);
       notifyListeners();
 
+  }
+
+  getArticlesByCategory(String category)async{
+
+      if( this.categoryArticles[category]!.length > 0){
+        return this.categoryArticles[category];
+      }
+
+      final url = Uri.parse('$_URL_NEWS/top-headlines?apiKey=$_APIKEY&country=ca&category=$category');
+      final resp = await http.get(url);
+
+      final newsResponse = newsResponseFromJson( resp.body );
+      this.categoryArticles[category]!.addAll( newsResponse.articles);
+      
+      notifyListeners();
   }
 }
