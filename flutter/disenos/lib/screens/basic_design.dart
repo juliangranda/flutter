2fd592e15c33c543640514b3e631148011613e38
juliangranda <juliangranda@hotmail.com>
2021-10-10 14:41:46 -0500

    diseño basico parte 2

--- flutter/disenos/lib/screens/basic_design.dart
+++ flutter/disenos/lib/screens/basic_design.dart
@@ -6,9 +6,46 @@ class BasicDesignScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Image(image: AssetImage('assets/lanscape.jpg'),),
+      body: Column(
+        children: [
+          //imagen
+          Image(image: AssetImage('assets/lanscape.jpg'),),
+
+          //titulo
+          Title()
+
+        ],
       )
+    );
+  }
+}
+
+class Title extends StatelessWidget {
+  const Title({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+      child: Row(
+
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('oeschinen lake', style: TextStyle(fontWeight: FontWeight.bold)),
+              Text('kandersteg, switzerland', style: TextStyle(color: Colors.black45),),
+            ],
+          ),
+          Expanded(
+            child: Container(),
+          ),
+          Icon(Icons.star, color: Colors.red,),
+          Text('41')
+        ],
+      ),
     );
   }
 }
