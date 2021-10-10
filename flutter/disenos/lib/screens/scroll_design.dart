7a664401db08ff55920237ce6cf8340c738b91dc
juliangranda <juliangranda@hotmail.com>
2021-10-10 15:35:55 -0500

    diseño con scroll parte 3

--- flutter/disenos/lib/screens/scroll_design.dart
+++ flutter/disenos/lib/screens/scroll_design.dart
@@ -1,20 +1,34 @@
 import 'package:flutter/material.dart';
 
 class ScrollScreen extends StatelessWidget {
-  
+  final boxDecoration = BoxDecoration(
+      gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          stops: [0.5, 0.5],
+          colors: [Color(0xff5EE8C5), Color(0xff30BAD6)]));
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children:[
+        body: Container(
+      decoration: boxDecoration,
+      child: PageView(
+        physics: BouncingScrollPhysics(),
+        scrollDirection: Axis.vertical,
+        children: [Page1(), Page2()],
+      ),
+    ));
+  }
+}
 
-          BackGround(),
-
-          MainContent(),
-        ]
-      )
-    );
+class Page1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(children: [
+      BackGround(),
+      MainContent(),
+    ]);
   }
 }
 
@@ -25,22 +39,33 @@ class MainContent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final textStyle = TextStyle(fontSize: 50, fontWeight: FontWeight.bold, color: Colors.white);
+    final textStyle = TextStyle(
+        fontSize: 50, fontWeight: FontWeight.bold, color: Colors.white);
     return SafeArea(
-      bottom:false,//no se preocupe poor la parte de abajo solo pon la restriccion en la parte de arriba
+      bottom:
+          false, //no se preocupe poor la parte de abajo solo pon la restriccion en la parte de arriba
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
-    
         children: [
-          SizedBox(height: 30)
-,          Text('11º', style: textStyle,),
-          Text('Miercoles', style: textStyle,),
+          SizedBox(height: 30),
+          Text(
+            '11º',
+            style: textStyle,
+          ),
+          Text(
+            'Miercoles',
+            style: textStyle,
+          ),
           //expanded
           Expanded(
             child: Container(),
           ),
-          Icon(Icons.keyboard_arrow_down, size:100, color: Colors.white,)
+          Icon(
+            Icons.keyboard_arrow_down,
+            size: 100,
+            color: Colors.white,
+          )
         ],
       ),
     );
@@ -48,13 +73,36 @@ class MainContent extends StatelessWidget {
 }
 
 class BackGround extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        color: Color(0xff30BAD6),
+        height: double.infinity,
+        alignment: Alignment.topCenter,
+        child: Image(
+          image: AssetImage('assets/scroll-1.png'),
+        ));
+  }
+}
 
+class Page2 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
       color: Color(0xff30BAD6),
-      height: double.infinity,
-      alignment: Alignment.topCenter,
-      child: Image( image: AssetImage('assets/scroll-1.png'),));
+      child: Center(
+        child: TextButton(
+          child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 40),
+              child: Text(
+                'bienvenido',
+                style: TextStyle(color: Colors.white, fontSize: 30),
+              )),
+          style: TextButton.styleFrom(
+              backgroundColor: Color(0xff0098FA), shape: StadiumBorder()),
+          onPressed: () {},
+        ),
+      ),
+    );
   }
 }
