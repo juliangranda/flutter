9fab61063cee37dd4ba5903cf1da93521a27e552
juliangranda <juliangranda@hotmail.com>
2021-10-09 19:39:54 -0500

    lista de categorias

--- flutter/newsapp/lib/src/services/news_service.dart
+++ flutter/newsapp/lib/src/services/news_service.dart
@@ -1,6 +1,8 @@
 //encargado de hacer peticiones http
 
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:newsapp/src/models/category_model.dart';
 import 'package:newsapp/src/models/news_models.dart';
 import 'package:http/http.dart' as http;
 
@@ -9,6 +11,17 @@ final _APIKEY = 'a76e3a6f008e4098bc5cee40a81c8524';
 class NewsService with ChangeNotifier{
 
   List<Article> headlines = [];
+
+  List<Category> categories = [
+    Category( FontAwesomeIcons.building, 'business'),
+    Category( FontAwesomeIcons.tv, 'entertainment'),
+    Category( FontAwesomeIcons.addressCard, 'general'),
+    Category( FontAwesomeIcons.headSideVirus, 'health'),
+    Category( FontAwesomeIcons.vials ,'science'),
+    Category( FontAwesomeIcons.volleyballBall, 'sports'),
+    Category( FontAwesomeIcons.memory, 'technology'),
+  ];
+  
 
   NewsService(){
 
