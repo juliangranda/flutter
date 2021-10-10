efb92487c833b892b5918d5155fa0212870948e3
juliangranda <juliangranda@hotmail.com>
2021-10-10 17:56:16 -0500

    anterior commit statusbar y titulos, actual commit bottom navigation bar

--- flutter/disenos/lib/screens/home_screen.dart
+++ flutter/disenos/lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:disenos/widgets/background.dart';
+import 'package:disenos/widgets/custom_bottom_navigation.dart';
 import 'package:disenos/widgets/page_title.dart';
 import 'package:flutter/material.dart';
 import 'package:disenos/widgets/background.dart';
@@ -19,6 +20,7 @@ class HomeScreen extends StatelessWidget {
           _HomeBody()
         ],
       ),
+      bottomNavigationBar: CustomBottomNavigation(),
     );
   }
 }
