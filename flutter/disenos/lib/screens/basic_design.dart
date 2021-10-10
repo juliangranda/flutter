997c51dd2e89e5ce0badeb8dc8f4a6a9bf6b96ae
juliangranda <juliangranda@hotmail.com>
2021-10-10 15:01:52 -0500

    diseño basico parte 3

--- flutter/disenos/lib/screens/basic_design.dart
+++ flutter/disenos/lib/screens/basic_design.dart
@@ -1,22 +1,31 @@
 import 'package:flutter/material.dart';
 
 class BasicDesignScreen extends StatelessWidget {
-  const BasicDesignScreen({ Key? key }) : super(key: key);
+  const BasicDesignScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          //imagen
-          Image(image: AssetImage('assets/lanscape.jpg'),),
+        body: Column(
+      children: [
+        //imagen
+        Image(
+          image: AssetImage('assets/lanscape.jpg'),
+        ),
 
-          //titulo
-          Title()
+        //titulo
+        Title(),
 
-        ],
-      )
-    );
+        //button section
+        ButtonSection(),
+
+        //descripcion
+        Container(
+          margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          child: Text(
+            'Do mollit magna veniam nostrud elit tempor et enim et reprehenderit id aute. Commodo pariatur elit sint tempor eiusmod est magna. Exercitation pariatur labore aute pariatur commodo. Quis elit incididunt consequat irure nostrud. Nulla nulla exercitation consectetur pariatur laboris aliqua in ea id. Ullamco pariatur enim eu nisi fugiat ut mollit Lorem. Elit in qui incididunt aliqua dolore nostrud sit mollit.'))
+      ],
+    ));
   }
 }
 
@@ -30,22 +39,74 @@ class Title extends StatelessWidget {
     return Container(
       margin: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
       child: Row(
-
         children: [
           Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text('oeschinen lake', style: TextStyle(fontWeight: FontWeight.bold)),
-              Text('kandersteg, switzerland', style: TextStyle(color: Colors.black45),),
+              Text('oeschinen lake',
+                  style: TextStyle(fontWeight: FontWeight.bold)),
+              Text(
+                'kandersteg, switzerland',
+                style: TextStyle(color: Colors.black45),
+              ),
             ],
           ),
           Expanded(
             child: Container(),
           ),
-          Icon(Icons.star, color: Colors.red,),
+          Icon(
+            Icons.star,
+            color: Colors.red,
+          ),
           Text('41')
         ],
       ),
+    );
+  }
+}
+
+class ButtonSection extends StatelessWidget {
+  const ButtonSection({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          CustomButton(icon: Icons.call, text: 'Call',),
+          CustomButton(icon: Icons.map, text: 'Route'),
+          CustomButton(icon: Icons.share, text: 'Share'),
+        ],
+      ),
+    );
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  final IconData icon;
+  final String text;
+
+  const CustomButton({Key? key, required this.icon, required this.text})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Icon(
+          this.icon,
+          color: Colors.blue,
+          size: 30,
+        ),
+        Text(
+          this.text,
+          style: TextStyle(color: Colors.blue),
+        )
+      ],
     );
   }
 }
