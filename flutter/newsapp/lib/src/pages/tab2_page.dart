cfba58d248edf37901e6509d8e5e3d70963e8fac
juliangranda <juliangranda@hotmail.com>
2021-10-09 19:21:13 -0500

    loading de los headlines y keepalive(se usa para mantener el estado permanente en un tab)

---
+++ flutter/newsapp/lib/src/pages/tab2_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class Tab2Page extends StatelessWidget {
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+         child: Text('homa mundo tab2'),
+      ),
+     
+    );
+  }
+}
--- flutter/newsapp/lib/src/pages/tabs_page.dart
+++ flutter/newsapp/lib/src/pages/tabs_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:newsapp/src/pages/tab1_page.dart';
+import 'package:newsapp/src/pages/tab2_page.dart';
 import 'package:provider/provider.dart';
 
 class TabsPage extends StatelessWidget {
@@ -50,9 +51,7 @@ class _Paginas extends StatelessWidget {
       children: [
         
         Tab1Page(),
-        Container(
-          color: Colors.green,
-        )
+        Tab2Page(),
       ],
     );
   }
